eae733c967a66b960fa5fe517822fa9bfe58434e
brayanespitia <brayanespitia2013@gmaail.com>
2021-12-29 01:21:51 -0500

    Initial commit

---
+++ src/main/java/com/retosofka/sofkapreguntas/SofkapreguntasApplication.java
@@ -0,0 +1,13 @@
+package com.retosofka.sofkapreguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkapreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkapreguntasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retosofka/sofkapreguntas/SofkapreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retosofka.sofkapreguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkapreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
